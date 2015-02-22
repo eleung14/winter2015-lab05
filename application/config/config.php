9c79f1bfe3c5368aee2f2b83781a7b17635bc372
Edward <ed.leung14@gmail.com>
2015-02-21 16:05:47 -0800

    FInished part b

--- application/config/config.php
+++ application/config/config.php
@@ -6,8 +6,10 @@ if (!defined('BASEPATH'))
 // the menu basics (text navbar)
 $config['menu_choices'] = array(
     'menudata' => array(
-	array('name' => "View 'Em", 'link' => '/viewer'),
-        )
+  array('name' => "View 'Em", 'link' => '/viewer'),
+  array('name' => "Change 'Em", 'link' => '/admin'),
+)
+		
     );
 
 
@@ -102,7 +104,7 @@ $config['charset'] = 'UTF-8';
   | setting this variable to TRUE (boolean).  See the user guide for details.
   |
  */
-$config['enable_hooks'] = FALSE;
+$config['enable_hooks'] = TRUE;
 
 
 /*
